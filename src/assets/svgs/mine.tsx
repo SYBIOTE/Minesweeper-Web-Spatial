93015a89a45dfa9654fb2f15116cfe843c0480df
sybiote <ghoshr698@gmail.com>
2025-09-23 00:30:11 +0000

    Refactor MineSVG component to accept style and className props for improved customization. Update SVG structure for better readability and maintainability.

--- src/assets/svgs/mine.tsx
+++ src/assets/svgs/mine.tsx
@@ -5,8 +5,28 @@ interface MineSVGProps {
   className?: string
 }
 
-const MineSVG: React.FC<MineSVGProps> = () => (
-  <svg viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#CCD6DD" d="M24.187 9.657l5.658-5.654L32 6.16l-5.658 5.655z"></path><circle fill="#ff0000" cx="14" cy="22" r="14"></circle><path fill="#ff0000" d="M19 11.342l5.658-5.657l5.657 5.658L24.657 17z"></path><circle fill="#F18F26" cx="32" cy="4" r="4"></circle><circle fill="#FDCB58" cx="32" cy="4" r="2"></circle></g></svg>
+const MineSVG: React.FC<MineSVGProps> = ({ style, className }) => (
+  <svg
+    viewBox="0 0 36 36"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    role="img"
+    className={className ? `iconify iconify--twemoji ${className}` : "iconify iconify--twemoji"}
+    preserveAspectRatio="xMidYMid meet"
+    fill="#000000"
+    style={style}
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path fill="#CCD6DD" d="M24.187 9.657l5.658-5.654L32 6.16l-5.658 5.655z"></path>
+      <circle fill="#ff0000" cx="14" cy="22" r="14"></circle>
+      <path fill="#ff0000" d="M19 11.342l5.658-5.657l5.657 5.658L24.657 17z"></path>
+      <circle fill="#F18F26" cx="32" cy="4" r="4"></circle>
+      <circle fill="#FDCB58" cx="32" cy="4" r="2"></circle>
+    </g>
+  </svg>
 )
 
 export default MineSVG
