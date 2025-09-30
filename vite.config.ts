e29964c591ec62958e2f485946d47a7eb42a003c
Rahul Ghosh <ghoshr698@gmail.com>
2025-09-30 16:13:46 -0700

    Add @vitejs/plugin-basic-ssl dependency and integrate SSL plugin in Vite configuration for enhanced security.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import react from '@vitejs/plugin-react'
 import webspatial from '@webspatial/vite-plugin'
 import { defineConfig } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    basicSsl(),
     react({
       jsxImportSource: '@webspatial/react-sdk'
     }),
